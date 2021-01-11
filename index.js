309c821a9c04b2762fbe9515f764709417c5fa18
seo-yeongjun <71090244+seo-yeongjun@users.noreply.github.com>
2021-01-12 03:49:59 +0900

    zz

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require("express");
+import "core-js";
+import express from "express";
 const app = express();
 const PORT = 4000;
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+const handleHome = (req, res) => res.send("hello");
+const handleprofile = (req, res) => res.send("It's my profile");
+app.get("/", handleHome);
+app.get("/profile", handleprofile);
 app.listen(PORT, handleListening);
